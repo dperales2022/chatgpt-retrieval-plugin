4a9ddf466a95f30c9783447aa3c31ca95600819b
Daniel Perales Carbonero <daniel.perales@atmira.com>
2024-03-25 08:56:29 +0100

    update main

--- server/main.py
+++ server/main.py
@@ -38,7 +38,7 @@ sub_app = FastAPI(
     title="Retrieval Plugin API",
     description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
     version="1.0.0",
-    servers=[{"url": "https://your-app-url.com"}],
+    servers=[{"url": "https://atmira-chatgpt-retrieval-plugin.onrender.com"}],
     dependencies=[Depends(validate_token)],
 )
 app.mount("/sub", sub_app)
